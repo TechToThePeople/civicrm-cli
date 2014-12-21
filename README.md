937446f8b24eaedef4f61de95609e2acd1c204d2
xavier <xavier@tttp.eu>
2014-12-21 23:45:39 +0100

    working enough to be useful

---
+++ README.md
@@ -0,0 +1,34 @@
+# CiviCRM command line
+This program allows you to search your civicrm contacts and print the result, from the comfort of your shell. 
+After you install it globally, it creates a program *civicrm" you can call from the cli.
+
+
+I wrote it because it's faster to type
+    $civicrm Xavier
+than going to a browser, authenticating, going to /civicrm and type on the search tool on the top left to get contact detail 
+
+you can have several civicrm sites you can query
+
+##installation
+$sudo npm -g install clicivi
+
+##configuration
+The configuration file contains all the key and api_keys of your site and is stored in ~/.config/civicrm.json
+
+The needed parameters (site url, key, api_key) are going to be prompted the first time you run the program or when you run 
+    $civicrm setup
+
+you can as well provide them from the command line
+    $civicrm setup -s example --server=https://example.org --key=yoursitekey --api_key=theapikeyofyouruser
+
+
+##usage
+    $civicrm -s example xavier
+    --> return the list of matching contacts
+    42: Dutoit, Xavier
+
+    $civicrm -s example view 42
+    Xavier Dutoit
+      +41 22 123 45 67
+      demo@example.org
+
---
+++ civicrm.js
@@ -0,0 +1,216 @@
+#!/usr/bin/env node
+'use strict';
+
+var program = require('commander');
+var prompt = require('prompt');
+var fs = require('fs');
+var clc = require('cli-color');
+
+var error = clc.red.bold;
+var warn = clc.yellow;
+var notice = clc.blue;
+var success = clc.green;
+
+var crmAPI = null;
+var configFile = {path:getUserHome()+'/.config/',name:'civicrm.json'}
+var config = {};
+
+program
+  .version('0.0.1')
+  .option("-s, --site [site]", "alias name for the site")
+  .option("-l,--server [url]", "url of the site")
+  .option("--api_key [key]", "api key for the user")
+  .option("--key [site_key]", "site key");
+
+
+config= readConfig(configFile);
+
+program
+  .command('setup')
+  .description('setup the configuration for the site')
+  .option("-s, --site [site]", "alias name for the site")
+  .option("--url [url]", "url the site")
+  .option("--key [key]", "api key for the user")
+  .option("--site_key [site_key]", "site key")
+  .action(function(){
+    var site = program.site || 'default';
+    console.log('creating configuration for site %s', site);
+    setupSite(site,configFile,{});
+});
+
+
+program
+  .option('-l, --limit', 'number of contacts to fetch (default 25)')
+.command('csv')
+.description('export the contacts as csv file')
+.action(function(env){
+  console.log(notice('export "%s"', env));
+});
+
+
+program
+.command('view [contact_id]')
+.description('view the detail of a contact')
+.action(function(id){
+  initCiviCRM ();
+  crmAPI.getSingle('contact', {
+    "sequential": 1,
+    "return": "display_name,organization_name",
+    "id": id,
+    "api.email.get": {},
+    "api.phone.get": {}
+  }, function(result) {
+   var c=result;
+   console.log (success(c.display_name +" from "+c.organization_name));
+   c["api.phone.get"].values.forEach(function(val) {
+     console.log(" "+val.phone);
+   });
+   c["api.email.get"].values.forEach(function(val) {
+     console.log(" "+val.email);
+   });
+  });
+});
+
+
+program
+  .option('-l, --limit', 'number of contacts to fetch (default 25)')
+.command('*')
+.description('search the site for contacts')
+.action(function(env){
+  initCiviCRM ();
+  console.log('searching "%s"', env);
+  crmAPI.getQuick ('contact',{name:env},
+    function (result) {
+      if (result.is_error) {
+        console.log(error("invalid result: "+result.error_message));
+        process.exit(1);
+      }
+      console.log(result.values);
+      for (var i in result.values) {
+        var val = result.values[i];
+        console.log(val.id +": "+val.sort_name+ " "+val.email+ " "+ val.phone);
+      }
+    });
+});
+
+program.parse(process.argv);
+
+
+
+function readConfig(file){
+  try {
+    var data = fs.readFileSync(file.path+file.name);
+  } catch (err) {
+    if ("ENOENT" == err.code) {
+      if (fs.existsSync(file.path)===false) {
+          fs.mkdirSync(file.path,'0700', function (err) { 
+            console.log (error("can't create directory "+ file.path));
+            process.exit(1);
+          });
+      }
+      fs.writeFileSync(file.path+file.name,"{}",'utf8');
+      console.log(warn('No configuration file found, creating a default site'));
+      setupSite("default",file,{});
+    }
+  }
+
+  try {
+      var c = JSON.parse(data);
+      if (c && c.length ==0) {
+        console.log(warn('configuration file '+file.path+file.name +' empty, creating a default site'));
+        setupSite("default",file,{});
+      }
+      return c;
+    }
+  catch (err) {
+    console.log (err);
+    console.log(error('No config file or error reading '+file.path+file.name));
+  }
+  return {};
+};
+
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
+
+function setupSite(name,file,options) {
+//  program.parse(process.argv);
+  name = name || "default";
+  if (process.site) program.name = process.site; // don't prompt for site name if set from -s [site]
+  options.path = options.path ||  '/sites/all/modules/civicrm/extern/rest.php';
+  options.server = options.server ||  program.server;
+  options.key = options.key ||  program.key;
+  options.api_key = options.api_key ||  program.api_key;
+
+  program.path = options.path; //don't ask for path
+
+  prompt.override = program;
+
+  prompt.start();
+
+  prompt.get([{
+      name: 'site',
+      default: name,
+      required: true
+    }, {
+      name: 'server',
+      required: true,
+      default: options.server,
+      message: 'url of the server eg. https://example.org/',
+    }, {
+      name: 'path',
+      required: true,
+      default: options.path
+    }, {
+      name: 'api_key',
+      required: true,
+      default: options.api_key,
+      message: "api key for the user to run the command as",
+       
+    }, {
+      name: 'key',
+      default: options.key,
+      message: "site key as defined in your civicrm.setting.php",
+      required: true
+       
+       
+    }], function (err, conf) {
+      crmAPI = require('civicrm')(conf);
+      crmAPI.get ('contact',{contact_type:'Individual',return:'display_name,email,phone'},
+        function (result) {
+          if (result.is_error) {
+            console.log(error("invalid configuration: "+result.error_message));
+            console.log(notice("try again: setup -s "+ conf.site + " --server="+ conf.server +" --key="+ conf.key +"--api_key="+conf.api_key));
+            process.exit(1);
+          }
+          config[conf.site] = conf;
+          fs.writeFileSync(file.path+file.name,JSON.stringify(config, null, 2),'utf8');
+          console.log(success("configuration saved in " + file.path+file.name));
+          /*
+          for (var i in result.values) {
+            var val = result.values[i];
+            console.log(val.id +": "+val.display_name+ " "+val.email+ " "+ val.phone);
+          }*/
+        });
+  });
+};
+
+function initCiviCRM () {
+  if (Object.keys(config).length > 0 ) {
+    if (program.hasOwnProperty("site")) { 
+      if (!config.hasOwnProperty(program.site)) { 
+        console.log (error("civicrm site "+ program.site + " not configured."));
+        console.log (warn("exisiting configurations "+ Object.keys(config).join(",")+" in "+ configFile.path+configFile.name));
+        console.log (notice("to create a new config $civicrm setup -s "+ program.site));
+        process.exit(1);
+
+      }
+      var c= config[program.site];
+      console.log (notice("using "+ program.site + " ("+c.server+")"));
+      crmAPI = require('civicrm')(c);
+    }
+  } 
+}
+
+
